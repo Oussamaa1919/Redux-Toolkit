0a810fa95f4c2339593e55f4af464e272438552d
Oussamaa1919 <medoussama.salhi@gmail.com>
2022-07-15 14:30:40 +0200

    commit

---
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import { ordered,restocked } from './cakeSlice'
+
+export const CakeView = () => {
+
+ const numOfCakes = useSelector((state)=> state.cake.numOfCakes)
+ const dispatch = useDispatch()
+ const [value,setValue] = React.useState(1)
+
+
+  return (
+    <div>
+      <h2>Number of cakes : {numOfCakes}</h2>
+      <button onClick={() => (dispatch(ordered()))}>Order cake</button>
+      <input
+      type='number'
+      value={value}
+      onChange={(e) => setValue(parseInt(e.target.value))}
+      
+      
+      />
+      <button onClick={() => (dispatch(restocked(value)))}>Restock cakes</button>
+    </div>
+  )
+}
+
+
---
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import { ordered,restocked } from '../icecream/icecreamSlice'
+
+export const IcecreamView = () => {
+
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams)
+  const dispatch = useDispatch()
+  
+  return (
+    <div>
+      <h2>Number of ice creams : {numOfIcecreams}</h2>
+      <button onClick={() => dispatch(ordered())}>Order ice cream</button>
+      <button onClick={() =>dispatch(restocked(5))}>Restock ice creams</button>
+    </div>
+  )
+}
+
